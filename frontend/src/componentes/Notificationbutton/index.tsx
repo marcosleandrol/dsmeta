471b841b06d0f0725032c071df5aa49356327476
Marcos Leandro <marcosleandrol@hotmail.com>
2022-09-07 21:54:00 -0300

    Primeiro componente

---
+++ frontend/src/componentes/Notificationbutton/index.tsx
@@ -0,0 +1,12 @@
+import icon from '../../assets/img/Vector.svg'
+import './style.css'
+
+function Notificationbutton() {
+    return (
+        <div className="dsmeta-red-btn">
+            <img src={icon} alt="Notificar" />
+        </div>
+    )
+}
+
+export default Notificationbutton
